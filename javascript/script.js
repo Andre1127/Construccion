31ce308de91241283bf6af3e553104ea761f3733
Andre1127 <141644058+Andre1127@users.noreply.github.com>
2024-01-08 19:53:58 -0500

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,4 @@
+// Verifica si la variable 'google' está definida
 if (typeof google !== 'undefined') {
                     // Inicializa la biblioteca de Google Sign-In
                    // Inicializa Google Identity Services y configura la función de devolución de llamada
@@ -7,8 +8,10 @@ if (typeof google !== 'undefined') {
                         cancel_on_tap_outside: false,
                     });
                 }
+//maneja la respuesta despues de una autenticacion
 function handleCredentialResponse(response) {
     if (response.credential) {
+    // Si hay una credencial en la respuesta
       var credential = response.credential;
       var profile = response.profileObj;
       // Maneja la información de la credencial como desees
@@ -16,6 +19,7 @@ function handleCredentialResponse(response) {
       console.log("Nombre:", credential.name);
       console.log("Email:", credential.email);
       console.log(response);
+      // Redirige a una URL específica después de manejar la respuesta
       window.location.href = 'https://www.youtube.com/watch?v=ld4nzao5XAc'
     }
   }
@@ -24,7 +28,7 @@ function handleSignOut() {
                     google.accounts.id.disableAutoSelect();
                     google.accounts.id.prompt();
                 }
-  
+// Verifica si la variable 'google' está definida
 if (typeof google !== 'undefined') {
                     // Renderiza el botón de inicio de sesión
                     google.accounts.id.renderButton(document.querySelector('.g_id_signin'));
